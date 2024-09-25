3ad6b86dd8ca15bca0cd9b4cd04cf46607955127
Dorenalto M Couto <dorenalto@gmail.com>
2024-09-25 20:51:07 -0300

    Inclusao de tela reativa alterando nome de classe para ficar mais sugestiva.

---
+++ Memorize/EmojiMemoryGameView.swift
@@ -0,0 +1,51 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by dorenalto mangueira couto on 25/09/24.
+//
+
+import SwiftUI
+
+struct EmojiMemoryGameView: View {
+    @ObservedObject var viewModel: EmojiMemoryGame
+    
+    var body: some View {
+        HStack {
+            ForEach(viewModel.cards) { card in
+                CardView(card: card).onTapGesture {
+                    self.viewModel.choose(card: card)
+                }
+            }
+        }
+        .padding()
+        .foregroundColor(Color.orange)
+    }
+}
+
+struct CardView: View {
+    var card: MemoryGame<String>.Card
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                if card.isFaceUp {
+                    RoundedRectangle(cornerRadius: cornerRadius).fill(.white)
+                    RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
+                    Text(card.content)
+                } else {
+                    RoundedRectangle(cornerRadius: cornerRadius).fill()
+                }
+            }
+            .font(Font.system(size: min(geometry.size.width, geometry.size.height) * fontScaleFactor))
+        }
+    }
+    //    MARK: - Drawing Constants
+    let cornerRadius: CGFloat = 10.0
+    let edgeLineWidth: CGFloat = 3
+    let fontScaleFactor: CGFloat = 0.75
+}
+
+#Preview {
+    EmojiMemoryGameView(viewModel: EmojiMemoryGame())
+}
