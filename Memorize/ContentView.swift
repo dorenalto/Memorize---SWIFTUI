7786d4df82278e9bf3d9d7a21709f252f41397dd
Dorenalto M Couto <dorenalto@gmail.com>
2024-09-25 11:40:10 -0300

    first commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by dorenalto mangueira couto on 25/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: false)
+            }
+        }
+        .padding()
+        .foregroundColor(.orange)
+        .font(.largeTitle)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10).fill(.white)
+                RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 3)
+                Text(" X ")
+            } else {
+                RoundedRectangle(cornerRadius: 10).fill()
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by dorenalto mangueira couto on 25/09/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
